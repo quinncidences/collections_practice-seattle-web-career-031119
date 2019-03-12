c6928e31216f3d395f42f6d97ddf058eecd96c13
Quinn Cox <qrc.inpa@gmail.com>
2019-03-12 00:44:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
+def sort_array_asc(array)
+  array.sort {|x,y| x<=>y}
+end
 
+
+def sort_array_desc(array)
+  array.sort {|x,y| y<=>x}
+end
+
+
+def sort_array_char_count(array)
+  array.sort_by {|word| word.length}
+end
+
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+return array
+end
+
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |word|
+    word_array = word.split ""
+    word_array[2] = "$"
+    new_array << word_array.join
+  end
+  new_array
+end
+
+
+def 
+    
+    
+    
+    
+    
